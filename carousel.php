44e7eb3b9eaf41a673f014e2ddcf55044f5f4052
Talha-Shekhani <60299561+Talha-Shekhani@users.noreply.github.com>
2020-07-20 21:17:48 +0500

    T_commit

---
+++ carousel.php
@@ -0,0 +1,26 @@
+<div class="col col-12 m-0 p-0">
+      <div id="mycarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <img src="assests/images/download.jpg" alt="utthapizza" class="d-block img-fluid">
+          </div>
+          <div class="carousel-item">
+            <img src="assests/images/download (2).jpg" alt="buffet" class="d-block img-fluid">
+          </div>
+          <div class="carousel-item">
+            <img src="assests//images/download (1).jpg" alt="alberto" class="d-block img-fluid">
+          </div>
+        </div>
+        <ul class="carousel-indicators">
+          <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#mycarousel" data-slide-to="1"></li>
+          <li data-target="#mycarousel" data-slide-to="2"></li>
+        </ul>
+        <a class="carousel-control-prev" data-target="#mycarousel" data-slide="prev" role="button">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" data-target="#mycarousel" data-slide="next" role="button">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+      </div>
+    </div>
